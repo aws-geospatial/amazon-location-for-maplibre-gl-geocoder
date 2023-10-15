a16e3b75f4881579e66c6cafbfaf79e0ad04a312
Brian Dyck <bridyck@amazon.com>
2023-10-14 19:39:45 -0500

    Added in additional configs for building into various versions of JS.

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
+
+const banner = `
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+// Third party license at
+`;
+
+export default {
+    input: "./dist/esm/index.js",
+    plugins: [
+        nodeResolve({
+            browser: true,
+        }),
+        json(),
+        commonjs(),
+    ],
+
+    output: [
+        {
+            file: "dist/amazonLocationMaplibreGeocoder.js",
+            format: "esm",
+            banner,
+            plugins: [
+                getBabelOutputPlugin({
+                    minified: true,
+                    moduleId: "amazonLocationAuthHelper",
+                    presets: [["@babel/env", { modules: "umd" }]],
+                }),
+            ],
+        },
+    ],
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { AmazonLocationMaplibreGeocoder } from "./AmazonLocationForMaplibreGeocoder";
+
+export { AmazonLocationMaplibreGeocoder };
